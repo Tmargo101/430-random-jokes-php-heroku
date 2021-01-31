fc1ba5b6439d830d369b5faf38dcdb5297dfc1de
Thomas Margosian <tmargo101@gmail.com>
2021-01-31 13:57:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 
 	// Jokes array
 	$jokes = [
-		["q"=>"What do you call a very small valentine?","a"=>"A valen-tiny!"],
-		["q"=>"What did the dog say when he rubbed his tail on the sandpaper?","a"=>"Ruff, Ruff!"],
-		["q"=>"Why don't sharks like to eat clowns?","a"=>"Because they taste funny!"],
-		["q"=>"What did the fish say when be bumped his head?","a"=>"Dam!"],
-		["q"=>"How many programmers does it take to change a light bulb?","a"=>"None, that's a hardware problem"],
-		["q"=>"Whats the object-oriented way to become wealthy?","a"=>"Inheritance!"],
-		["q"=>"Why did the programmer quit his job?","a"=>"Because he didn't get arrays!"],
-		["q"=>"Why are Assembly programmers always soaking wet?","a"=>"They work below C-level!"],
-		["q"=>"What is the most used language in programming?","a"=>"Profanity!"],
-		["q"=>"How did the programmer die in the shower?","a"=>"He read the shampoo bottle instructions: Lather. Rinse. Repeat."],
-		["q"=>"How do you tell HTML from HTML5?","a"=>"Try it out in Internet Explorer.  Did it work?  No?  It's HTML5!"]
+		["q"=>"What do you call a very small valentine?","a"=>"A VALEN-TINY!"],
+		["q"=>"What did the dog say when he rubbed his tail on the sandpaper?","a"=>"RUFF, RUFF!"],
+		["q"=>"Why don't sharks like to eat clowns?","a"=>"BECAUSE THEY TASTE FUNNY!"],
+		["q"=>"What did the fish say when be bumped his head?","a"=>"DAM!"],
+		["q"=>"How many programmers does it take to change a light bulb?","a"=>"NONE, THAT'S A HARDWARE PROBLEM"],
+		["q"=>"Whats the object-oriented way to become wealthy?","a"=>"INHERITANCE!"],
+		["q"=>"Why did the programmer quit his job?","a"=>"BECAUSE HE DIDN'T GET ARRAYS!"],
+		["q"=>"Why are Assembly programmers always soaking wet?","a"=>"THEY WORK BELOW C-LEVEL!"],
+		["q"=>"What is the most used language in programming?","a"=>"PROFANITY!"],
+		["q"=>"How did the programmer die in the shower?","a"=>"HE READ THE SHAMPOO BOTTLE INSTRUCTIONS: LATHER. RINSE. REPEAT."],
+		["q"=>"How do you tell HTML from HTML5?","a"=>"TRY IT OUT IN INTERNET EXPLORER.  DID IT WORK?  NO?  IT'S HTML5!"]
 	];
 
 	// Set default value for limit (if $_GET param is not passed)
